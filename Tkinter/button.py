3aad2ccaa3c4f2c2381c4141054a5eff6e6199fb
hai saori <haisaori@gmail.com>
2023-04-06 17:32:03 +0700

    add radioButton

--- Tkinter/button.py
+++ Tkinter/button.py
@@ -9,6 +9,7 @@ window.geometry('600x400')
 # button
 def func_button():
     print('a basic button')
+    print(radio_var.get())
 button_string = tk.StringVar(value='A button with string var')
 button = ttk.Button(window, text='A Simple Button', command=func_button, textvariable=button_string)
 button.pack()
@@ -24,6 +25,24 @@ check = ttk.Checkbutton(
     offvalue=5
     )
 check.pack()
+
+# radio buttons
+radio_var = tk.StringVar()
+radio1 = ttk.Radiobutton(
+    window,
+    text = 'RadioButton 1',
+    value='radio 1',
+    variable=radio_var,
+    command=lambda: print(radio_var.get())
+    )
+radio1.pack()
+radio2 = ttk.Radiobutton(
+    window,
+    text='RadioButton 2',
+    value=2,
+    variable=radio_var
+    )
+radio2.pack()
 
 # run
 window.mainloop()
